6c5d5f350e2ab49b038e40b776615e405d77039e
Jimmy Trulock <ttrulock23@gmail.com>
2023-07-11 14:45:42 -0600

    add append code and testing

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,11 +1,55 @@
-require "./lib/linked_list"
+require './lib/linked_list.rb'
 
-class Beatbox
+class BeatBox
     attr_accessor :head, :next_node, :data, :list
 
     def initialize
         @list = LinkedList.new
     end
+
+    def is_empty?
+        if @head == nil
+             true
+        else
+             false
+        end
+    end  
+
+    def append(data)
+        bucket = data.split
+        
+        if self.is_empty?
+            list.head = Node.new(bucket.shift)
+            current_node = list.head
+
+            while bucket != []
+                current_node.next_node = Node.new(bucket.shift)
+                current_node = current_node.next_node
+                end       
+        else
+            new_node = Node.new(data)
+            while current_node.next_node != nil
+                current_node = current_node.next_node
+            end
+            current_node.next_node = new_node
+        end
+
+    end
+
+    def count
+        if self.is_empty?
+            count = 0
+        else
+            count = 1
+            current_node = list.head
+            while current_node.next_node != nil
+                current_node = current_node.next_node
+                count += 1
+            end
+        end
+        count
+    end
+
 end
 
 
--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -2,10 +2,22 @@ require "./lib/beat_box.rb"
 
 describe Beatbox do
     it "creates a class with an empty list" do
-        bb = Beatbox.new
+        bb = BeatBox.new
         head_check = bb.list.head
 
         expect(bb).to be_an_instance_of(Beatbox)
         expect(head_check).to eq(nil)
     end
 end
+
+describe "#append" do
+    it "adds multiple nodes to linkedlist" do
+        bb = BeatBox.new
+        bb.append("deep doo ditt")
+
+
+        expect(bb.list.head.data).to eq("deep")
+        expect(bb.list.head.next_node.data).to eq("doo")
+    end
+end
+
